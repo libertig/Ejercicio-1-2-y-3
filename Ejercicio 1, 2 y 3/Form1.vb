31a7ddc234fe1c180c03e825920b48d1bd495b35
Gabriel Liberti <45043938+libertig@users.noreply.github.com>
2019-06-10 10:02:11 -0300

    Add files via upload

---
+++ Ejercicio 1, 2 y 3/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Module1.Mensaje = TextBox1.Text
+        Form2.Show()
+    End Sub
+End Class
---
+++ Ejercicio 1, 2 y 3/Module1.vb
@@ -0,0 +1,3 @@
+﻿Module Module1
+    Public Mensaje As String
+End Module
